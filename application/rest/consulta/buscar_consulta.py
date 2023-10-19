e9dad08a811a6d99132ca424d7a7ef4b6558c676
Victor Alexandre <victoralexandre29051986@gmail.com>
2023-10-19 15:27:31 -0300

    Finalizado application consulta

---
+++ application/rest/consulta/buscar_consulta.py
@@ -0,0 +1,80 @@
+import json
+from http import HTTPStatus
+
+from flask import Response
+from flask_restx import Namespace, Resource
+from pydantic import ValidationError
+
+from modules.consulta.controller import ConsultaController
+
+api_buscar_consulta = Namespace("consulta", description="Endpoint buscar consulta")
+
+
+@api_buscar_consulta.route("/<int:id>", methods=["GET"])
+class BuscarConsultaPorId(Resource):
+
+    def get(self, id: int):
+        try:
+            response = ConsultaController.buscar_consulta_por_id(id)
+            return Response(
+                response.json(),
+                mimetype="application/json",
+                status=200,
+            )
+
+        except ValidationError as exc:
+            return Response(
+                exc.json(),
+                mimetype="application/json",
+                status=HTTPStatus.BAD_REQUEST
+            )
+
+
+        except ValueError as exc:
+            return Response(
+                json.dumps({'msg': exc.args[0]}),
+                mimetype="application/json",
+                status=HTTPStatus.BAD_REQUEST
+            )
+
+
+        except Exception as exc:
+            return Response(
+                json.dumps({"msg": 'Bad request'}),
+                mimetype="application/json",
+                status=HTTPStatus.BAD_REQUEST
+            )
+
+
+@api_buscar_consulta.route("/", methods=["GET"])
+class BuscarConsultas(Resource):
+
+    def get(self):
+        try:
+            response = ConsultaController.buscar_consultas()
+            return Response(
+                json.dumps(response),
+                mimetype="application/json",
+                status=200,
+            )
+
+        except ValidationError as exc:
+            return Response(
+                exc.json(),
+                mimetype="application/json",
+                status=HTTPStatus.BAD_REQUEST
+            )
+
+        except ValueError as exc:
+            return Response(
+                json.dumps({'msg': exc.args[0]}),
+                mimetype="application/json",
+                status=HTTPStatus.BAD_REQUEST
+            )
+
+        except Exception as exc:
+            return Response(
+                json.dumps({"msg": 'Bad request'}),
+                mimetype="application/json",
+                status=HTTPStatus.BAD_REQUEST
+            )
