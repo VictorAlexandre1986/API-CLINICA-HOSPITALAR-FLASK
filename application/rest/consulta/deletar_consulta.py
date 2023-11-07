4eeed386558cf48e2153976c7b72d63978a75ad3
Victor Alexandre <victoralexandre29051986@gmail.com>
2023-11-07 15:11:29 -0300

    Finalizado application consulta

---
+++ application/rest/consulta/deletar_consulta.py
@@ -0,0 +1,47 @@
+import json
+from http import HTTPStatus
+
+from flask import Response, request
+from flask_restx import Namespace, Resource
+from pydantic import ValidationError
+
+from modules.consulta.controller import ConsultaController
+
+api_deletar_consulta = Namespace("consulta", description="Endpoint deletar consulta")
+
+
+@api_deletar_consulta.route("/<int:id>", methods=["DELETE"])
+class DeletarConsulta(Resource):
+
+    def delete(self, id: int):
+        try:
+            ConsultaController.deletar_consulta(id)
+            return Response(
+                json.dumps({"msg": "Excluído com sucesso."}),
+                mimetype="application/json",
+                status=200,
+            )
+
+        except ValidationError as exc:
+            return Response(
+                exc.json(),
+                mimetype="application/json",
+                status=HTTPStatus.BAD_REQUEST
+            )
+
+
+        except ValueError as exc:
+            return Response(
+                json.dumps({'msg': exc.args[0]}),
+                mimetype="application/json",
+                status=HTTPStatus.BAD_REQUEST
+            )
+
+
+        except Exception as exc:
+            print(exc)
+            return Response(
+                json.dumps({"msg": 'Bad request'}),
+                mimetype="application/json",
+                status=HTTPStatus.BAD_REQUEST
+            )
